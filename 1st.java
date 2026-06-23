c4dde17bd5f4ef0b974f72239c9aca682e95b04c
lingarajum <lingarjum551@gmail.com>
2026-06-23 14:43:37 +0530

    java code altered

--- 1st.java
+++ 1st.java
@@ -1,5 +1,9 @@
 class 1st{
+    void display(){
+        System.out.println("This is My Message")
+    }
     public static void main(String[] args){
         System.out.println("raju");
+
     }
 }
